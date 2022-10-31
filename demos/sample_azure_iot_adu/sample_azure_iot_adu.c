ac85b4ea458897063e6c2f206cd51487d044ac99
Jan Martens <jan.martens@flandermake.be>
2022-10-31 09:48:25 +0100

    Dirty fix for json in json bug.

--- demos/sample_azure_iot_adu/sample_azure_iot_adu.c
+++ demos/sample_azure_iot_adu/sample_azure_iot_adu.c
@@ -134,7 +134,8 @@
  */
 #define ADU_HEADER_BUFFER_SIZE                                512
 
-#define democonfigADU_UPDATE_ID                               "{\"provider\":\"" democonfigADU_UPDATE_PROVIDER "\",\"name\":\"" democonfigADU_UPDATE_NAME "\",\"version\":\"" democonfigADU_UPDATE_VERSION "\"}"
+#define democonfigADU_UPDATE_ID                               democonfigADU_UPDATE_VERSION
+//#define democonfigADU_UPDATE_ID                               "{\"provider\":\"" democonfigADU_UPDATE_PROVIDER "\",\"name\":\"" democonfigADU_UPDATE_NAME "\",\"version\":\"" democonfigADU_UPDATE_VERSION "\"}"
 
 #ifdef democonfigADU_UPDATE_NEW_VERSION
     #define democonfigADU_SPOOFED_UPDATE_ID                   "{\"provider\":\"" democonfigADU_UPDATE_PROVIDER "\",\"name\":\"" democonfigADU_UPDATE_NAME "\",\"version\":\"" democonfigADU_UPDATE_NEW_VERSION "\"}"
