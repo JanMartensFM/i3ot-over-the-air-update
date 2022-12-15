6c6e468d964616e53d560339747e9584e2a728f9
Jan Martens <jan.martens@flandermake.be>
2022-12-15 11:29:25 +0100

    Added update of version number in import manifest.

--- demos/projects/ESPRESSIF/adu/calculate_hash.py
+++ demos/projects/ESPRESSIF/adu/calculate_hash.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import base64
 import hashlib
 
@@ -11,7 +12,7 @@ def get_file_hash(file_path):
         return base64.b64encode(hashlib.sha256(bytes).digest()).decode("utf-8")
 
 
-def update_manifest(firmwarefile, manifest_file_name, manifest_template_filename):
+def update_manifest(firmwarefile, manifest_file_name, manifest_template_filename, version_number):
     manifest_template = open(manifest_template_filename, "r")
 
     manifest_template_contents = manifest_template.read()
@@ -27,6 +28,7 @@ def update_manifest(firmwarefile, manifest_file_name, manifest_template_filename
 
     manifest_template_contents = manifest_template_contents.replace("{{sizeInBytes}}", str(file_size))
     manifest_template_contents = manifest_template_contents.replace("{{hashCode}}", str(file_hash))
+    manifest_template_contents = manifest_template_contents.replace("{{versionNumber}}", version_number)
 
     print (manifest_template_contents)
 
@@ -38,9 +40,23 @@ def update_manifest(firmwarefile, manifest_file_name, manifest_template_filename
 
 # Parameters for this deployment
 firmware_file = "demos/projects/ESPRESSIF/adu/azure_iot_freertos_esp32.bin"
-manifest_file = "demos/projects/ESPRESSIF/adu/ESPRESSIF.ESP32-Azure-IoT-Kit.1.9.importmanifest.json"
+manifest_file = "demos/projects/ESPRESSIF/adu/ESPRESSIF.ESP32-Azure-IoT-Kit.importmanifest.json"
 manifest_template = "demos/projects/ESPRESSIF/adu/ESPRESSIF.ESP32-Azure-IoT-Kit.importmanifest.template.json"
 
+# Use this for debugging.
+# firmware_file = "ESPRESSIF.ESP32-Azure-IoT-Kit.importmanifest.template.json"
+# manifest_file = "ESPRESSIF.ESP32-Azure-IoT-Kit.importmanifest.json"
+# manifest_template = "ESPRESSIF.ESP32-Azure-IoT-Kit.importmanifest.template.json"
 
-update_manifest(firmware_file, manifest_file, manifest_template)
 
+
+# First argument should be the version number
+if len(sys.argv) == 1:
+    print ("The version number to use was not passed on the command line.")
+    exit()
+
+version_number = sys.argv[1]
+
+print (f"Settting version number to {version_number}.")
+
+update_manifest(firmware_file, manifest_file, manifest_template, version_number)
